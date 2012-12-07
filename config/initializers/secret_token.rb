85503d0a4ff8e9e4e2ab09cc27ef7769aee4b610
prashant85 <dixitprashant855@gmail.com>
2012-12-07 16:35:30 +0530

    first step toward walmart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Walmartshowroom::Application.config.secret_token = '712c99927e5de65df4d0425c1265ddab0e8d8e729c5bded7b25e8a807e8b71f435306c1f1930e7880e5eceff37470a6acbe9d63e8cbd55c351e2f00f7ed7b85b'
